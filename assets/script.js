e180806e9f736692a5660d29c2d74682582615d1
Georgia Hunt <georgialhunt89@gmail.com>
2021-04-06 10:00:00 +0930

    Funct for click listener on saveBtn & local stora

--- assets/script.js
+++ assets/script.js
@@ -2,4 +2,14 @@
 const todayDate = dayjs().format('dddd, MMMM D, YYYY h:mm A');
 $("#currentDay").html(todayDate);
 
+// Function for click listener on saveBtn
+$(document).ready(function (){
+    $('.saveBtn').on('click', function (){
+        const text = $(this).siblings('.description').val();
+        const time = $(this).parent().attr('id');
+
+        //Save local time and text input in local storage
+        localStorage.setItem(time, text);
+    })
+})
 
