8a29fea4b3037bf79e6f65f0f459a26e5d61710c
Georgia Hunt <georgialhunt89@gmail.com>
2021-04-06 11:17:00 +0930

    Added call for timeTracker function

--- assets/script.js
+++ assets/script.js
@@ -54,5 +54,8 @@ $(document).ready(function (){
     $('#hour15 .description').val(localStorage.getItem('hour15'));
     $('#hour16 .description').val(localStorage.getItem('hour16'));
     $('#hour17 .description').val(localStorage.getItem('hour17'));
+
+    // Call time tracker function
+    timeTracker();
 })
 
